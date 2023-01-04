a50368c88b7aa62133cf7d5485af73b25ee30b2f
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-04 08:04:01 -0800

    py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -7,4 +7,4 @@ for i in range(0, 10):
         if i == 8 and j == 9:
             print("{:d}{:d}".format(i, j))
         else:
-            print("{:d}{:d}".format(i,j), end=", ")
+            print("{:d}{:d}".format(i, j), end=", ")
