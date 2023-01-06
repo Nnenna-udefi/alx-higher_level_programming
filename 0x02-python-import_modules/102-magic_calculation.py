ff15e094514a166c401d94625892a0307f3acd45
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-06 12:55:21 -0800

    module calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
-from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for n in range(4, 6):
