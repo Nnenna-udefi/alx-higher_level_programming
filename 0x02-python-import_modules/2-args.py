c517c5c9a4fdb4ea5a8e7422e7d855c2396f6153
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-06 16:03:36 -0800

    var

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
     n = len(sys.argv) - 1
     if n == 0:
-        print("{:d} argument.".format(n))
+        print("{:d} arguments.".format(n))
     elif n == 1:
         print("{:d} argument:".format(n))
     else:
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    from variable_load_5 import a
+from variable_load_5 import a
 
+def find_variable():
     print(a)
+
+if __name__ == "__main__":
+    find_variable()
