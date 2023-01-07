7ef012136633dbf93f0c5136fc20cd4840837ef6
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-07 14:05:54 -0800

    tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,4 +4,4 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         sentence[0] = None
     else:
-        return len(sentence), sentence[0]
+        return (len(sentence), sentence[0])
