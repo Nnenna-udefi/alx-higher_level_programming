c295b38cc3267d67c4264f27ff1d55241912cfa0
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-06 16:16:56 -0800

    variable

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-from variable_load_5 import a
-
-def find_variable():
-    print(a)
-
 if __name__ == "__main__":
-    find_variable()
+    from variable_load_5 import a
+    print(a)
