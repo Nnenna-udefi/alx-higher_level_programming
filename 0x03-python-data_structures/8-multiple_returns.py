9f1d8fa1e5852fe7e9bfdaf6fc3f34e272e584e2
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-10 10:33:56 -0800

    string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
+    sentence[0] = first
     if len(sentence) == 0:
-        sentence[0] = "None"
+        first = None
     else:
-        return (len(sentence), sentence[0])
+        return (len(sentence), first)
