eb7533e0abf574edd2bf68e6aa26098a7610c5ce
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-10 10:44:03 -0800

    tuple error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,7 +6,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = (0, 0)
     elif len(tuple_a) < 2:
         tuple_a = (tuple_a[0], 0)
-    elif len(tuple_b) < 1:
+    if len(tuple_b) < 1:
         tuple_b = (0, 0)
     elif len(tuple_b) < 2:
         tuple_b = (tuple_b[0], 0)
