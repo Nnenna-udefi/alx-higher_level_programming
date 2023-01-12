1baa25bc76b1754cfc8d49be2603f3f819d39678
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-12 15:55:16 -0800

    roman

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list - function thatprints some basic info about python lists
+ * @p: list
+ */
+void print_python_list(PyObject *p)
+{
+	int i;
+
+
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if roman_string is not "" or None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
+    roman_i = {
+            'I': 1,
+            'V': 5,
+            'X': 10, 
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+    }
+    inte = 0
+    for i in range(len(roman_string)):
+         if i > 0 and roman_i[roman_string[i]] > roman_i[roman_string[i - 1]]:
+            inte += roman_i[roman_string[i]] - 2 * roman_i[roman_string[i - 1]]
+         else:
+             inte += roman_to_int[roman_string[i]]
+    return inte
