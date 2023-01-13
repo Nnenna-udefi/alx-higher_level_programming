131e1f107356b20e1ed1a68f4ac788703e6453c3
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-13 08:26:22 -0800

    key

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,6 +5,6 @@ def update_dictionary(a_dictionary, key, value):
         a_dictionary[key] = value
     else:
         for i in a_dictionary:
-            i == key
-            a_dictionary[i] = value
+            if i == key:
+                a_dictionary[i] = value
     return a_dictionary
