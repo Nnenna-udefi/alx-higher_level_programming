3bf641d1249932988769888145b96ae6b7512dca
Nnenna Udefi <miriamudefi@gmail.com>
2023-02-11 21:26:46 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines a Recctanle class"""
+"""Defines a Rectangle class"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Represents a rectangle that inherits the projects base"""
+    """Represents a rectangle that inherits the project's base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes the Rectangle
