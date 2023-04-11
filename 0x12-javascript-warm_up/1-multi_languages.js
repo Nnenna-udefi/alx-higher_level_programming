bd435e0ea983052475443a70441034d27f91f6c9
Nnenna Udefi <miriamudefi@gmail.com>
2023-04-11 13:51:19 -0700

    javascript arguments

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const script1 = 'C is fun';
+console.log(script1);
+
+const script2 = 'Python is cool';
+console.log(script2);
+
+const script3 = 'JavaScript is amazing';
+console.log(script3);
