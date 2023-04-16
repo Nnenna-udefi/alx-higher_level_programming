1506ca693eea4c2892c368e14c994da9b5498e5b
Nnenna Udefi <miriamudefi@gmail.com>
2023-04-16 08:19:58 -0700

    filter

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-    script that takes in an argument and displays all values
-    in the states table of hbtn_0e_0_usa where
-    name matches the argument
-"""
+"""Lists all the cities from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
@@ -12,7 +8,7 @@ if __name__ == '__main__':
             db=sys.argv[3], port=3306)
 
     curr = db.cursor()
-    curr.execute("SELECT * FROM states WHERE name = '{}';".format(sys.argv[4]))
+    curr.execute("SELECT * FROM cities;")
     states = curr.fetchall()
 
     for state in states:
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that takes in the name of a state as an argument
+    and lists all cities of that state,
+    using the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], \
+            db=sys.argv[3], port=3306)
+
+    curr = db.cursor()
+    curr.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = '{}';".format(sys.argv[4[]))
+    states = curr.fetchall()
+
+    for state in states:
+        print(state)
