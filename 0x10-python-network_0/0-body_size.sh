5e401b9db96c6d8ea27694bc1ee8f41def4db99a
Nnenna-udefi <miriamudefi@gmail.com>
2023-04-27 05:16:36 -0700

    content-size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
 url="$1"
-curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
+curl -s "$url" | wc -c
