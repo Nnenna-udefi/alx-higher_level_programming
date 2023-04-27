124b238118de0dbadb549051641434acdbf346ce
Nnenna-udefi <miriamudefi@gmail.com>
2023-04-27 04:29:14 -0700

    curl

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# -w option specifies the format of the output and returns the size of the response body in bytes
+# -s option tells curl to operate in silent mode
+
+url="$1"
+curl -s -w "$url" 
