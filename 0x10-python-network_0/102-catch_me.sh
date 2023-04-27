5df3d22312910074fd089d041ca3ae930a4651d2
Nnenna Udefi <miriamudefi@gmail.com>
2023-04-27 10:31:44 -0700

    catch_me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -s -o /dev/null -w "You got me!" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
