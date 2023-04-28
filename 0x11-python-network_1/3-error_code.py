bcbbe424f655127f2ff493b3b64e9a0bda61c432
Nnenna Udefi <miriamudefi@gmail.com>
2023-04-28 14:25:18 -0700

    error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    script that takes in a URL, sends a request to the URL and displays
+    the body of the response (decoded in utf-8).
+"""
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+            print(body.encode("UTF-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
