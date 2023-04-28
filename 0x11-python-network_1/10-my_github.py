ec3b13ce073e4a173037b3e501f9bff5e17b6c4f
Nnenna Udefi <miriamudefi@gmail.com>
2023-04-28 16:09:09 -0700

    github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    script that takes your GitHub credentials (username and password)
+    and uses the GitHub API to display your id
+"""
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+if __name__ == '__main__':
+    url = "https://api.github.com/user"
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    r = requests.get(url, auth=(HTTPBasicAuth('username', 'password')))
+    if r.status_code == 200:
+        res_json = r.json()
+        print(res_json.get("id"))
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
     script that takes in a URL, sends a request to the URL and
     displays the body of the response.
 """
