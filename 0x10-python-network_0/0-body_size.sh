046148d820071688353968e4e3e028b7582b3ec9
Nnenna Udefi <miriamudefi@gmail.com>
2023-04-27 23:04:15 -0700

    wc

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-url="$1"
-curl -s "$url" | wc -c
+# script that takes in a URL, sends a request to that URL, and displays the size of the response body
+curl -s "$1" | wc -c
