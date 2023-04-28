d285133b4057c1da27c64be3dbfc36318dc38778
Nnenna Udefi <miriamudefi@gmail.com>
2023-04-28 15:54:41 -0700

    json_api

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,15 +3,16 @@
     script that takes in a URL, sends a request to the URL and displays
     the body of the response (decoded in utf-8).
 """
-from urllib import request, error
 import sys
+import urllib.request
+import urllib.error
 
 if __name__ == '__main__':
     url = sys.argv[1]
 
     try:
-        with request.urlopen(url) as response:
+        with urllib.request.urlopen(url) as response:
             body = response.read()
             print(body.encode("UTF-8"))
-    except error.HTTPError as e:
+    except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
