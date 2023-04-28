ddf57bd4f0cb38a2eb3dc204109485041cf82bae
Nnenna Udefi <miriamudefi@gmail.com>
2023-04-28 14:37:18 -0700

    requests

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,8 +3,7 @@
     script that takes in a URL, sends a request to the URL and displays
     the body of the response (decoded in utf-8).
 """
-import urllib.request
-import urllib.error
+from urllib import request, error
 import sys
 
 if __name__ == '__main__':
